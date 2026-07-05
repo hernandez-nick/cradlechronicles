f7ce79f54e82f0474d070ebcc6d2591cffa19479
hernandeznick-ga <hernandeznick1990@gmail.com>
2026-07-04 21:58:22 -0700

    initial setup

---
+++ cradlechronicles/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cradlechronicles project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cradlechronicles.settings')
+
+application = get_asgi_application()
