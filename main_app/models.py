b6962155a2a193d5f9136516440114d3cd391fb4
hernandeznick-ga <hernandeznick1990@gmail.com>
2026-07-07 01:07:02 -0700

    added milestone model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Milestone(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
